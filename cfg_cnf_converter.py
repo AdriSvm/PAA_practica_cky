34019db113f36b03b719306fc2340fa14e5b5bc3
AdriSvm <adria.svm@gmail.com>
2023-05-11 15:56:42 +0200

    class cky implementation

---
+++ cfg_cnf_converter.py
@@ -0,0 +1,226 @@
+import nltk
+from nltk import CFG, Nonterminal, Production
+
+
+class ChomskyConverter:
+    def __init__(self, grammar:CFG) -> None:
+        self.grammar = grammar
+        self.non_terminal_count = 0
+
+
+    def convert_cfg(self) -> nltk.grammar:
+        """
+        :return: CFG gramatic converted to CNF
+        """
+        if not self.is_chomsky_normal_form():
+            self.remove_unitaries()
+            self.remove_unreachable()
+            self.remove_3mprods()
+            self.change_terminals()
+        return self.grammar
+
+    def is_chomsky_normal_form(self) -> bool:
+        """
+        :return: bool whether the self.grammar is or not in chomsky normal form (CNF)
+        """
+        return all([not self.are_unitary_rules(),not self.are_3mprods(),
+                        not self.are_more_terminals(),not self.are_malformed_terminals()])
+    def is_unitary_nt(self,prod:Production) -> bool:
+        """
+        :param prod: object of nltk.Production
+        :return: Whether the production is unitary and, it's rhs is Nonterminal
+        """
+        return len(prod.rhs()) == 1 and isinstance(prod.rhs()[0],Nonterminal)
+
+    def is_unitary(self,prod:Production) -> bool:
+        """
+        :param prod: object of nltk.Production
+        :return: Whether the production is or not unitary. Doesn't affect if it's Nonterminal or Terminal
+        """
+        return len(prod.rhs()) == 1
+
+    def are_unitary_rules(self):
+        """
+        :return: Whether there are or not Unitary rules of Nonterminals in self.grammar
+        """
+        for prod in self.grammar.productions():
+            if len(prod.rhs()) == 1 and isinstance(prod.rhs()[0],Nonterminal):
+                return True
+        return False
+
+    def rhss(self,prods:list[Production]) -> list:
+        """
+        :param prods: List of Production objects from nltk
+        :return: All rhs() of all the productions in a list
+        """
+        rhss = []
+        for p in prods:
+            for i in p.rhs():
+                rhss.append(i)
+        return rhss
+
+    def remove_unitaries(self) -> None:
+        """
+        Removes all the unitary Nonterminal productions in the self.grammar object
+        :return: None
+        """
+        while self.are_unitary_rules():
+            prods = []
+            for prod in self.grammar.productions():
+                if self.is_unitary_nt(prod):
+                    for pr in self.grammar.productions(lhs=prod.rhs()[0]):
+                        prods.append(Production(prod.lhs(),pr.rhs()))
+
+                elif prod not in prods:
+                    prods.append(prod)
+
+            if prods:
+                self.grammar = CFG(self.grammar.start(), prods)
+
+    def grammar_BFS(self) -> set:
+        """
+        :return: All the productions in a BFS order of the self.grammar object
+        """
+        reachable = set()
+        reachable.add(self.grammar.start())
+
+        queue = [self.grammar.start()]
+        while queue:
+            symbol = queue.pop(0)
+            productions = self.grammar.productions(lhs=symbol)
+            for production in productions:
+                for rhs in production.rhs():
+                    if not rhs in reachable:
+                        reachable.add(rhs)
+                        queue.append(rhs)
+        return reachable
+    def remove_unreachable(self) -> None:
+        """
+        Removes all unreachable productions of the self.grammar object
+        :return: None
+        """
+        print('a')
+        reachable_symbols = self.grammar_BFS()
+
+        new_productions = [p for p in self.grammar.productions() if p.lhs() in reachable_symbols and all(
+            s in reachable_symbols or isinstance(s, str) for s in p.rhs())]
+
+        if new_productions:
+            self.grammar = CFG(self.grammar.start(),new_productions)
+
+    def are_3mprods(self) -> bool:
+        """
+        :return: Whether there are 3 or more symbols in the right hand of any production of self.grammar object
+        """
+        for prod in self.grammar.productions():
+            if len(prod.rhs()) > 2:
+                return True
+        return False
+    def remove_3mprods(self) -> None:
+        """
+        Processes all productions with more than 3 symbols in its right hand of the object self.grammar
+        :return: None
+        """
+        counter_nt = 0
+
+        while self.are_3mprods():
+            new_prods = []
+            for prod in self.grammar.productions():
+                if len(prod.rhs()) > 2:
+                    new_nt = Nonterminal(f"X{counter_nt}")
+                    counter_nt += 1
+                    new_prods.append(Production(lhs=prod.lhs(),rhs=[prod.rhs()[0],new_nt]))
+                    new_prods.append(Production(lhs=new_nt,rhs=[i for i in prod.rhs()[1:]]))
+
+                else:
+                    new_prods.append(prod)
+
+            if new_prods:
+                self.grammar = CFG(start=self.grammar.start(),productions=new_prods)
+
+    def are_more_terminals(self,production:Production=None):
+        """
+        :param production: A Production object of nltk
+        :return: bool whether the production or the self.grammar have mo
+        """
+        if production:
+            return not all([isinstance(i,Nonterminal) for i in production.rhs()])
+
+        for prod in self.grammar.productions():
+            if not all([isinstance(i,Nonterminal) for i in prod.rhs()]):
+                return True
+        return False
+
+    def are_malformed_terminals(self):
+        for prod in self.grammar.productions():
+            if len(prod.rhs()) > 1 and not all([isinstance(i,Nonterminal) for i in prod.rhs()]):
+                return True
+        return False
+    def change_terminals(self):
+        counter_nt = 0
+        new_prods = []
+        for pr in self.grammar.productions():
+            if not self.is_unitary(pr) and self.are_more_terminals(production=pr):
+                rhss = []
+                for symbol in pr.rhs():
+                    if isinstance(symbol,str):
+                        new_nt = Nonterminal(f"Y{counter_nt}")
+                        counter_nt += 1
+
+                        new_prods.append(Production(lhs=new_nt,rhs=[symbol]))
+                        rhss.append(new_nt)
+                    else:
+                        rhss.append(symbol)
+                new_prods.append(Production(lhs=pr.lhs(),rhs=rhss))
+
+            elif pr not in new_prods:
+                new_prods.append(pr)
+
+        if new_prods:
+            self.grammar = nltk.CFG(start=self.grammar.start(),productions=new_prods)
+
+
+
+grammar = CFG.fromstring("""
+    S -> A
+    A -> B
+    B -> C
+    C -> D
+    D -> E
+    E -> F
+    F -> G
+    G -> 'a'
+    
+""")
+grammar = CFG.fromstring("""
+    S -> A B
+    A -> 'a'
+    B -> C
+    C -> 'c'
+    D -> 'd'
+""")
+grammar = CFG.fromstring("""
+    S -> A C
+A -> B
+B -> D
+C -> 'd'
+D -> E F G
+E -> 'a' F
+F -> 'b' G
+G -> 'c'
+H -> 'd'
+""")
+
+
+cfg = ChomskyConverter(grammar)
+print(cfg.is_chomsky_normal_form())
+print(cfg.grammar)
+cfg.remove_unitaries()
+print(cfg.grammar)
+cfg.remove_unreachable()
+print(cfg.grammar)
+cfg.remove_3mprods()
+print(cfg.grammar)
+cfg.change_terminals()
+print(cfg.grammar)
+print(cfg.grammar.is_chomsky_normal_form())
--- models.py
+++ models.py
@@ -1,23 +1,37 @@
 import nltk
+from nltk import PCFG
+from cfg_cnf_converter import ChomskyConverter
 
 class CKY():
 
     def __init__(self):
+        """
+        Initialisation of the class, just put some variables to it's initial state
+        """
         self._grammar = None
         self._phrase = []
         self._n = 0
         self._table = None
 
+        self._pgrammar = None
+        self._probabilities = []
+
     def _initialise_grammar(self,grammar:str) -> None:
-        self._grammar = nltk.CFG.fromstring(grammar)
-        if not self._grammar_is_cnf():
-            cnf_gram = self._chomsky_normal_form()
-            if cnf_gram:
-                self._grammar = cnf_gram
-            else:
-                raise Exception('Grammar could not be converted to CNF form')
-            
+        """
+        :param grammar: string with a CFG(Context Free Grammar), may be or not in CNF(Chomsky Normal Form)
+        :return: None, saves the grammar in class variables
+        """
+        G = ChomskyConverter(nltk.CFG.fromstring(grammar))
+        self.grammar = G.convert_cfg()
+
+    def _initialise_probabilistic_grammar(self,grammar:str):
+        self._pgrammar = PCFG.fromstring(grammar)
+
     def _initialise_phrase(self,phrase) -> None:
+        """
+        :param phrase: A sentence in string format or a list of words
+        :return: None, saves to class variables
+        """
         if type(phrase) is list:
             self._phrase = phrase
         elif type(phrase) is str:
@@ -25,56 +39,160 @@ class CKY():
         else:
             raise Exception("Phrase in incorrect format")
 
-    def _initialise_diagonal_table(self):
+    def _initialise_diagonal_table(self) -> bool:
+        """
+        Initialises the first values of the dynamic table. These are the nonterminal symbols(lhs)
+        associated to terminal ones(rhs)
+        :return: A bool indicating that the initialisation has been successful
+        """
         for i, word in enumerate(self._phrase):
+            if len(self._grammar.productions(rhs=word)) == 0:
+                return False
             self._table[i][i] = set(rule.lhs() for rule in self._grammar.productions(rhs=word))
 
-    def _initialise_table(self) -> None:
+        return True
+
+    def _initialise_table(self) -> bool:
+        """
+        Initialises the table of dimensions len(self._phrase)*len(self._phrase)
+        with empty sets and calls the initialisation of the main diagonal
+        :return: A bool indication that the creation has been successful
+        """
         if self._grammar and len(self._phrase) > 0:
             self._n = len(self._phrase)
             self._table = [[set() for _ in range(self._n)] for _ in range(self._n)]
-            self._initialise_diagonal_table()
+            if not self._initialise_diagonal_table():
+                return False
+            return True
         else:
             self._n = 0
             self._table = [[]]
+            return False
+
+    def initialise_ptable(self) -> None:
+        self._probabilities = [[{} for _ in range(self._n)] for _ in range(self._n)]
 
 
-    def _grammar_is_cnf(self) -> bool:
-        if self._grammar:
-            return self._grammar.is_chomsky_normal_form()
-        return False
-
-    def _chomsky_normal_form(self) -> bool:
-        if self._grammar:
-            return self._grammar.chomsky_normal_form()
-        return False
-
-    def _bottom_up_cky(self):
+    def _bottom_up_cky(self) -> None:
+        """
+        Main method to call for executing the bottom_up CKY algorithm with Dynamic Programming
+        :return: None, saves the result to the self._table object
+        """
         # Llenar la tabla CKY de manera recursiva
         if self._grammar and self._table and self._n > 0:
             for j in range(0, self._n):
                 for i in range(j - 1, -1, -1):
-                    for k in range(i, j + 1):
-                        print(self._table)
+                    for k in range(i, j):
                         left_cell = self._table[i][k]
                         right_cell = self._table[k + 1][j]
-                        for i in self._grammar.productions(rhs=(left_cell, right_cell)):
-                            self._table[i, j].add(i.lhs())
+                        for left in left_cell:
+                            for right in right_cell:
+                                for z in self._grammar.productions(rhs=left):
+                                    print(z.rhs())
+                                    if len(z.rhs()) > 1 and z.rhs()[1] == right:
+                                        self._table[i][j].add(z.lhs())
 
     def cky_parse(self,words:list, grammar:str) -> bool:
         """
-        Implementación del algoritmo CKY para determinar si una oración cumple con una gramática.
+        Initialises the parser which tells whether a sentence complies with a grammar through the
+        deterministic CKY algorithm
 
-        Parameters:
-        words (list): Lista de palabras de la oración.
-        grammar (nltk.CFG): Gramática en formato Chomsky normal.
+        :param words (list): List of words in the sentence.
+        :param grammar (nltk.CFG): Grammar in normal Chomsky format.
 
-        Returns:
-        bool: True si la oración cumple con la gramática, False en caso contrario.
+        :return: bool: True if the sentence complies with the grammar, False otherwise.
         """
         self._initialise_grammar(grammar=grammar)
         self._initialise_phrase(phrase=words)
-        self._initialise_table()
-
-        # La oración cumple con la gramática si S está en la celda (0, self._n-1)
+        if not self._initialise_table():
+            return False
+        self._bottom_up_cky()
+        # The sentence complies with the grammar if S is in cell (0, self._n-1)
         return nltk.grammar.Nonterminal('S') in self._table[0][self._n - 1]
+
+    def _parse_with_probabilities(self):
+            for j in range(0, self._n):
+                for i in range(j - 1, -1, -1):
+                    for k in range(i, j):
+                        left_cell = self._table[i][k]
+                        right_cell = self._table[k + 1][j]
+                        for left in left_cell:
+                            for right in right_cell:
+                                for z in self._pgrammar.productions(rhs=left):
+                                    if z.rhs()[1] == right:
+                                        prob = z.prob()
+                                        if z.lhs() not in self._probabilities[i][j]:
+                                            self._probabilities[i][j][z.lhs()] = prob
+                                        else:
+                                            self._probabilities[i][j][z.lhs()] = max(self._probabilities[i][j][z.lhs()], prob)
+
+            return self._probabilities[0][self._n - 1]
+    def ckyp_parse(self,words,grammar):
+        """
+        Initialises the parser which tells whether a sentence complies with a grammar through the
+        probabilistic CKY algorithm
+
+        :param words (list): List of words in the sentence.
+        :param grammar (nltk.CFG): Grammar in normal Chomsky format.
+
+        :return: bool: True if the sentence complies with the grammar, False otherwise.
+        """
+        self._initialise_probabilistic_grammar(grammar=grammar)
+        self._initialise_phrase(phrase=words)
+        self._i
+        self._parse_with_probabilities()
+        # The sentence complies with the grammar if S is in cell (0, self._n-1)
+        return nltk.grammar.Nonterminal('S') in self._table[0][self._n - 1]
+
+
+    def joc_de_proves(self):
+        grammar = """
+            S -> NP VP
+            NP -> Det N
+            VP -> V NP
+            Det -> 'the' | 'a'
+            N -> 'cat' | 'dog' | 'table' | 'house'
+            V -> 'chases' | 'sleeps' | 'sits'
+        """
+        sentences = [
+            "the cat chases a dog",
+            "a dog sleeps on the table",
+            "the house sits on the table",
+            "a cat chases the dog",
+            "the dog sleeps on a house"
+        ]
+        for sentence in sentences:
+            if self.cky_parse(sentence.split(), grammar):
+                print(f"'{sentence}' cumple con la gramática")
+            else:
+                print(f"'{sentence}' no cumple con la gramática")
+
+
+    def joc_de_proves2(self):
+        grammar = """
+                    S -> 'a' | X A | A X | 'b'
+                    A -> R B
+                    B -> A X | 'b' | 'a'
+                    X -> 'a'
+                    R -> X B
+                """
+        grammar = """
+                S -> A B | A C
+                A -> 'a' D
+                B -> 'b'
+                C -> 'c'
+                D -> 'g'
+                """
+        sentences = [
+            "abc",
+            "aabb"
+        ]
+        for sentence in sentences:
+            if self.cky_parse(list(sentence), grammar):
+                print(f"'{sentence}' cumple con la gramática")
+            else:
+                print(f"'{sentence}' no cumple con la gramática")
+
+
+a = CKY()
+a.joc_de_proves2()
